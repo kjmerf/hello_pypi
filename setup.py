cfe22ca09c38c1aff89d55e570151f10cdb9582f
Kevin Merfeld <kevin.merfeld@zoro.com>
2020-01-26 16:00:55 -0600

    setup changes

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="hello-kjmerf",
-    version="0.0.6",
+    version="0.0.7",
     author="Kevin Merfeld",
     author_email="kevinjmerfeld@gmail.com",
     description="A package that says hello to kjmerf",
-    long_description='This package says hello to kjmerf!',
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kjmerf/hello_kjmerf",
-    download_url="https://github.com/kjmerf/hello_kjmerf/archive/v_0_0_6.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
