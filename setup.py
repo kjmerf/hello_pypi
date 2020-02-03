e2daa700674d67c9c3647189d236ee3668aeaf4c
Kevin Merfeld <kevin.merfeld@zoro.com>
2020-02-03 10:24:41 -0600

    moving test file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="hello-kjmerf",
-    version="2.0.1",
+    version="2.0.2",
     author="Kevin Merfeld",
     author_email="kevinjmerfeld@gmail.com",
     description="A package that says hello to kjmerf",
