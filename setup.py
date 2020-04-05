c80d73ff5c3be776571c816fe7c69b9026071b66
Kevin Merfeld <kevin.merfeld@zoro.com>
2020-04-05 11:54:16 -0500

    reverting update

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 import setuptools
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="hello-kjmerf",
-    version="2.0.4",
+    version="2.0.5",
     author="Kevin Merfeld",
     author_email="kevinjmerfeld@gmail.com",
     description="A package that says hello to kjmerf",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/kjmerf/hello_kjmerf",
     packages=setuptools.find_packages(),
     classifiers=[
