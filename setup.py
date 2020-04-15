0416a59fb80a1bc86dafdfebb457d8bf5ec207bb
Kevin Merfeld <kevin.merfeld@zoro.com>
2020-04-14 23:03:45 -0500

    updating version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="hello-kjmerf",
-    version="2.0.8",
+    version="2.0.9",
     author="Kevin Merfeld",
     author_email="kevinjmerfeld@gmail.com",
     description="A package that says hello to kjmerf",
